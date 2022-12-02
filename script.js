145972a48cc9db7abfb4b398e1119af709d81acc
Harsh Prajapati <prajapati.captain@gmail.com>
2022-12-02 10:54:26 +0530

    css update

--- script.js
+++ script.js
@@ -1,36 +1,29 @@
-let hours = document.getElementById("hours");
-let minutes = document.getElementById("minutes");
-let seconds = document.getElementById("seconds");
-let AMPM = document.getElementById("AMPM");
+
 
 setInterval(() => {
-  let clock = new Date();
-  let hrs = clock.getHours();
-  let min = clock.getMinutes();
-  let sec = clock.getSeconds();
-
-  if (hrs > 12) {
-    hrs = hrs - 12;
-  }
-  if (hrs < 10) {
-    hrs = "0" + hrs;
-  }
-  if (min < 10) {
-    min = "0" + min;
-  }
-  if (sec < 10) {
-    sec = "0" + sec;
-  }
-  if (hrs == 0) {
-    hrs = 12;
-  }
-  if (hrs < 12) {
-    AMPM.innerHTML = "PM";
-  } else {
-    AMPM.innerHTML = "AM";
-  }
-
-  hours.innerHTML = hrs;
-  minutes.innerHTML = min;
-  seconds.innerHTML = sec;
-});
+    // Elements From HTML Body
+    let ss = document.getElementById("second")
+    let mm = document.getElementById("minute")
+    let hh = document.getElementById("hourss")
+    let ampm = document.getElementById("am")
+  
+    // Date Variables
+    let date = new Date();
+    let h = date.getHours();
+    let m = date.getMinutes();
+    let s = date.getSeconds();
+    m = (m < 10) ? "0" + m : m
+    s = (s < 10) ? "0" + s : s
+  
+    // AM or PM
+    let am = h >= 12 ? "PM" : "AM";
+    // Converting From 24Hr Format to 12Hr
+    if (h > 12) {
+      h = h - 12
+    }
+    h = (h < 10) ? "0" + h : h
+    hh.innerHTML = h;
+    mm.innerHTML = m;
+    ss.innerHTML = s;
+    ampm.innerHTML = am;
+  })
